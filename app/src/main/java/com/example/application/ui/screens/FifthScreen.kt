0bd9c26070cbe6292acc811a14e60a3915af6b3b
casalderrey <mariadelmar.casalderrey@gmail.com>
2022-12-14 20:53:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/application/ui/screens/FifthScreen.kt
@@ -0,0 +1,10 @@
+package com.example.application.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.application.ui.screens.scaffold.AppScaffold
+
+@Composable
+fun FifthScreen(navController: NavController){
+    AppScaffold(navController, {})
+}
